9003c27ed60e4bd8489a64d2fab3abd2058da8bd
root <root@amit.(none)>
2013-10-10 12:08:22 +0530

    Bash Automation script for wireshark

---
+++ instant-script/tshark/readme.rst
@@ -0,0 +1,4 @@
+Automate the Analysis process
+=============================
+
+Bash script with SED and AWK is use for the automation process.
---
+++ instant-script/tshark/testscript/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+	
+	mkdir ClientUploadTest
+	
+	cd ClientUploadTest
+ServerIp=10.129.200.120
+HostIp=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+# tshark to capture the file 
+   	tshark -i eth0 -a duration:120  host $ServerIp -w CapFileTest.pcap
+# converting to .csv with desired fields   
+	tshark -r CapFileTest.pcap -T fields -e ip.src -e frame.len -E separator=, -E occurrence=f>CapFileTestSrc.csv
+# Deleting the server IP
+	sed -n "/$ServerIp/!p" CapFileTestSrc.csv>CapFileTestClient.csv
+# Generate Averaqge Packet per second by calcluate total packets transmit in time
+#	cat CapFileTestClient.csv | cut -d ',' -f 2 | paste -d   - > ClinetFLen.csv
+   
+	echo `cat CapFileTestClient.csv | cut -d ','  -f 2 | paste -sd+| bc`/`cat CapFileTestClient.csv|wc -l` | bc -l>AvgPacketSize.csv
+
+#  	awk '{sum +=$1} END {print sum / NR}' ClinetFLen.csv>AvgPacketSize.csv
+# Generate total packets tranmitted by client. 
+  
+ 	awk 'END {print NR}' CapFileTestClient.csv >TotalClientPacket.csv
+# Generate Average Packet size
+	awk '{avgPckt=$1/120} {print avgPckt}' TotalClientPacket.csv>AvgPacketSec.csv
+# Generate Upload bandwidth of client
+  	cat AvgPacketSize.csv|awk '{n=$1;getline<"AvgPacketSec.csv";print n*$1}'>BwdthKbps.csv
+# For Download Bandwidth	
+	
+	mkdir ../ClientDownLoadTest
+	cp CapFileTestSrc.csv ../ClientDownLoadTest
+	cd ../ClientDownLoadTest
+	sed -n "/$HostIp/!p" CapFileTestSrc.csv>CapFileTestClient.csv 
+	echo `cat CapFileTestClient.csv | cut -d ','  -f 2 | paste -sd+| bc`/`cat CapFileTestClient.csv|wc -l` | bc -l>AvgPacketSizeDown.csv
+
+	awk 'END {print NR}' CapFileTestClient.csv >TotalClientPacketDown.csv
+	awk '{avgPckt=$1/120} {print avgPckt}' TotalClientPacketDown.csv>AvgPacketSecDown.csv
+	cat AvgPacketSizeDown.csv|awk '{n=$1;getline<"AvgPacketSecDown.csv";print n*$1}'>BwdthKbpsDown.csv
