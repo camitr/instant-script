b1dd136ff14c84c329293157450979681fd785fc
root <root@amit.(none)>
2013-10-23 11:18:58 +0530

    change the name

--- instant-script/tshark/testscript/sshpython.py
+++ instant-script/tshark/testscript/sshpython.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
 import paramiko
+hostname = raw_input('IP:')
+#dur	= raw_input('duration:')
+#username = raw_input('username:')
+#print hostname
+#print username
+ 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
-ssh.connect('10.129.200.93', username='admin', 
-    password='ttt23$')
+ssh.connect(hostname,username='admin',password='ttt23$')
+print "Analysing the packets......."
 stdin,stdout,stderr=\
 ssh.exec_command("./BndwithAnalysis.sh")
 type(stdin)
